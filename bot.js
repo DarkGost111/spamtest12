32aa5aca144f3a4a931a6adc6b78d0e68945e33c
DarkGost111 <45178136+DarkGost111@users.noreply.github.com>
2018-12-19 20:43:23 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ console.log("Scrpit By Legend");
 client.on("ready", () => {
 let channel =     client.channels.get("487745714227511316")
 setInterval(function() {
-channel.send(`#credits ! | ℓєɢєиɒ ⁹⁹☕#6178 68000`);
+channel.send(`#credits @! | ℓєɢєиɒ ⁹⁹☕#6178 68000`);
 }, 30)
 })
 
