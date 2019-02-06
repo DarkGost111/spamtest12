eceed72f67d06d34793db2299b4f4f14d6df4f95
DarkGost111 <45178136+DarkGost111@users.noreply.github.com>
2019-02-06 21:34:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,11 +3,6 @@ const client = new Discord.Client();
 console.log("Scrpit By Legend");
 
 
-client.on("ready", () => {
-let channel =     client.channels.get("524852684583469079")
-setInterval(function() {
-channel.send(`خروف العيد يوسف`);
-}, 30)
-})
-
-client.login(process.env.BOT_TOKEN);
+client.on('guildMemberAdd', member=> {
+    member.addRole(member.guild.roles.find("name","Tornados"));
+    });
